28857f33391518102e57d7e875d7d3907e071bcb
Matheus Barros <matheusbsousa95@gmail.com>
2022-02-22 22:38:26 -0300

    Correção conquistas

--- src/exercicios_projeto_jedi/conquista01/Exercicio02.java
+++ src/exercicios_projeto_jedi/conquista01/Exercicio02.java
@@ -1,7 +1,7 @@
 package exercicios_projeto_jedi.conquista01;
 
 public class Exercicio02 {
-
+//TODO Corrigir: As classes devem estar fora do escopo (-> { } <-) da classe Exercicio02
     class Moto{
 
     }
